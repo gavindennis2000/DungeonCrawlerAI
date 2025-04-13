70b5cfe4d9149cb3c3a38a57ca06d9b0d0a07395
gavindennis2000 <135553311+gavindennis2000@users.noreply.github.com>
2025-04-12 19:05:28 -0500

    refactoring

--- dungeon-crawler-ai/src/main.jsx
+++ dungeon-crawler-ai/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 import UserText from './UserText.jsx'
 import StoryText from './StoryText.jsx'
 import ImageDisplay from './ImageDisplay.jsx'
@@ -8,6 +9,7 @@ import DungeonCrawler from './DungeonCrawler.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <App />
     <DungeonCrawler />
   </StrictMode>,
 )
